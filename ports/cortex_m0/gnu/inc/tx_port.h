91a193638be02601c0e11ebca1f88125839e2385
Vinzenz Petr <vinzenz_82@yahoo.de>
2024-05-30 18:00:20 +0000

    added missing typedef for ULONG64

--- ports/cortex_m0/gnu/inc/tx_port.h
+++ ports/cortex_m0/gnu/inc/tx_port.h
@@ -88,6 +88,7 @@ typedef int                                     INT;
 typedef unsigned int                            UINT;
 typedef long                                    LONG;
 typedef unsigned long                           ULONG;
+typedef unsigned long long                      ULONG64;                       
 typedef short                                   SHORT;
 typedef unsigned short                          USHORT;
 
